099855a39659a9e8224bf0c84b9fe93bcc8c0997
Enzo Hamelin <enzo.hamelin@gmail.com>
2015-05-21 09:40:32 +0200

    Main initial

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/**
+ * Programme principal du FindClone
+ *
+ * @author Enzo Hamelin
+ * @author Amirali Ghazi
+ * @author Clément Thierry
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+  
+	return EXIT_SUCCESS;
+}
